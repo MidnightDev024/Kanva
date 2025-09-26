9b60e3bb15838417bab7e106f7af23f23c6c30c0
MidnightDev024 <chawlaakash024@gmail.com>
2025-09-26 22:59:38 +0530

    add: LoginPage.jsx

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1 className='border w-full h-screen sm:px-[15%] sm-py-[5%]'>
+        
+      </h1>
+    </div>
+  )
+}
+
+export default LoginPage
