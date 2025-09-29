8035b68eb950e8f1ac8676a4c2077ceb22fae2d4
MidnightDev024 <chawlaakash024@gmail.com>
2025-09-30 00:12:18 +0530

    refactored: assets and sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { userDummyData } from '../assets/assets';
+import asset, { userDummyData } from '../assets/assets';
 
 const Sidebar = ({ selectedUser, setSelectedUser }) => {
 
@@ -21,15 +21,27 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
           </div>
         </div>
 
-        <div className='bg-[#282142] rounded-full flex item-center gap-2 py-3 px-4 mt-5'>
+        <div className='bg-[#282142] rounded-full flex item-center gap-2 py-3 px-4 mt-0'>
           <img src='./src/assets/search_icon.png' alt="search" className='w-3' />
           <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search User...'/>
         </div>
 
         <div className='flex flex-col'>
           {userDummyData.map((user, index) => {
-            <div>
-              <img src="{user?.profilePic || assets.avatar_icon}" alt="" />
+            <div onClick={()=> {setSelectedUser(user)}} key={index} className={`relative flex item-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && '[#282142]/50'}`}>
+              <img src="{user?.profilePic || assets.avatar_icon}" alt="profilePic" className='w-[35px] aspect-[1/1] rounded-full' />
+              <div className='flex flex-col leading-5'>
+                <p>{user.fullName}</p>
+                {
+                  index < 3
+                  ? <span className='text-green-400 text-xs'>Online</span>
+                  : <span className='text-green-400 text-xs'>Offline</span>
+                }
+              </div>
+              {
+                index > 2
+                && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center item-center rounded-full bg-voilet-500/50'>{index}</p>
+              }
             </div>
           })}
         </div>
