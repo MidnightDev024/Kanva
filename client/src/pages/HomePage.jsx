748e774535b1669123cabe0e3448c335abc6f79f
MidnightDev024 <chawlaakash024@gmail.com>
2025-09-30 23:32:51 +0530

    Added: props for SelsctUser

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -10,9 +10,9 @@ const HomePage = () => {
   return ( 
     <div className='border w-full h-screen sm:px-[15%] sm-py-[5%] grid place-items-center'>
         <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[80%] grid grid-cols-1 relative w-full ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-          <Sidebar />
-          <ChatContainer />
-          <RightSideBar />
+          <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+          <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+          <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
         </div>
     </div>
   )
