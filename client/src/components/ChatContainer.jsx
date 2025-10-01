8338f6111261656af9eba567ea44747b01eae9be
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-01 13:23:32 +0530

    refactored the right side bar added profile pic name media logout button

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -16,9 +16,9 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
       {/* ---------- Header -------------- */}
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
-        <img src={assets.profile_martin} alt="User Profile Pic" className='w-8 rounded-full'/>
+        <img src={selectedUser.profilePic} alt="User Profile Pic" className='w-8 rounded-full'/>
         <p className='flex-1 text-lg text-white flex items-center gap-2'>
-          Martin Johnson
+          {selectedUser.fullName}
           <span className='w-2 h-2 rounded-full bg-green-500'></span>
         </p>
         <img onClick={() => setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import assets, { imagesDummyData } from '../assets/assets';
 
-const RightSideBar = () => {
-  return (
-    <div>   
-      <h1>Right Side Bar</h1>
+const RightSideBar = ({ selectedUser }) => {
+  return selectedUser ? (
+    <div className='relative bg-[#8185B2]/10 h-full p-5 rounded-l-xl text-white'>
+      {/* ------profile info----------- */}
+      <div className='flex flex-col items-center gap-4'>
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full' />
+        <div className='text-center'>
+          <h1 className='text-xl font-medium flex items-center justify-center gap-2'>
+            {selectedUser.fullName}
+            <span className='w-2 h-2 rounded-full bg-green-500'></span>
+          </h1>
+          <p className='text-gray-300 mt-2'>{selectedUser.bio}</p>
+        </div>
+      </div>
+      <hr className='border-[#fffffff50] my-4' />
+      {/* ------------media----------- */}
+      <div className='px-5 text-xs'>
+        <p>Media</p>
+        <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
+          {imagesDummyData.map((url, index)=> (
+            <div key={index} onClick={()=> window.open(url)} className='cursor-pointer rounded'>
+              <img src={url} alt="" className='h-full rounded-md' />
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* ---------logout---------- */}
+      <button className='absolute bottom-5 left-1/2 -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white text-sm font-light py-2 px-20 rounded-full cursor-pointer'>
+        Logout
+      </button>
     </div>
-  )
+  ) : null
 }
 
 export default RightSideBar
