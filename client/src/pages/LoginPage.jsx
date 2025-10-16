da80915e022eb3823cb9dfc5c87994cf0e7b654a
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-16 21:58:54 +0530

    refactored: LoginPage.jsx for final

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -81,21 +81,30 @@ const LoginPage = () => {
 
   const [isDataSubmited, setIsDataSubmited] = React.useState(false)
 
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if(currState === "Sign up" && !isDataSubmited){
+      setIsDataSubmited(true);
+      return;
+    }
+  }
+
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
       {/* --------Left----------- */}
       <img src={assets.logo_big} alt="" className='w-[max(250px)]'/>
 
       {/* --------Right----------- */}
-      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+      <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
         <h2 className='font-medium text-2xl flex justify-between items-center'>
           {currState}
-          <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />
-          </h2>
+          {isDataSubmited && <img onClick={()=> setIsDataSubmited(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />}
+        </h2>
 
           {currState === "Sign up" && !isDataSubmited && (
             
-          <input onChange={(e)=>setFullName(e.target.value)} value={fullName} type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required/>
+          <input onChange= {(e)=>setFullName(e.target.value)} value={fullName} type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required/>
           )}
           
           {!isDataSubmited && ( 
@@ -109,12 +118,12 @@ const LoginPage = () => {
             <textarea onChange={(e)=>setBio(e.target.value)} value={bio} rows={4} className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' placeholder='Provide a Short bio... ' required></textarea>
           )}   
 
-          <button className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
-            {currState === "Sign Up" ? "Create Account" : "Login Now"}
+          <button type='submit' className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
+            {currState === "Sign up" ? "Create Account" : "Login Now"}
           </button>
 
           <div className='flex item-center gap-2 text-sm text-gary-500'>
-            <input type="checkbox"  />
+            <input type="checkbox" required/>
             <p>Agree to terms of use and privacy policy</p>
           </div>
 
