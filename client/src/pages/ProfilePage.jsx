728d8f84946a1e26f8a80b2b4335e46f46b525a3
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-18 23:16:23 +0530

    refactored:  profilepage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -8,7 +8,7 @@ const ProfilePage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const navigate = useNavigate();
   const [name, setName] = useState("Martin Smith");
-  const [bio, setBio] = useState("Hi, I'm Martin. A passionate developer and tech enthusiast. I am using  ");
+  const [bio, setBio] = useState("Hi, I'm Martin. I am using Kanva chat application.");
 
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'> 
@@ -16,8 +16,8 @@ const ProfilePage = () => {
           <form className='flex flex-col gap-5 p-10 flex-1'>
             <h3 className='text-lg'>Profile Detail</h3>
             <label htmlFor="avtar" className='flex items-center gap-3 cursor-pointer'></label>
-            <input onChange={(e)=>setSelectedImage(e.target.files)} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
-            <img src={""} alt="" />
+            <input onChange={(e)=>setSelectedImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+            <img src={selectedImage ? URL.createObjectURL(selectedImage) : assets.avatar} alt="" className={`w-12 h-12`} />
           </form>
           <img src="" alt="" />
         </div>
