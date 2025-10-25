01222c83961d4638a854fa8b88ca40cea98f26ed
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-25 18:57:36 +0530

    refactored: server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 import express from 'express';
-import "dotenv/config";
+import 'dotenv/config';
 import cors from 'cors';
-import http, { Server } from 'http';
+import http from 'http';
+import { log } from 'console';
 
-// create express app and http server
+// create an express app and an http server
+
 const app = express();
 const server = http.createServer(app);
 
-// middleware setup
-app.use(express.json({limit: '4mb'}));
-app.use(cors());
+// Middleware setup
 
-app.use("/api/status", (req, res) => res.send("Server is running..."));
+app.use(express.json({limit: "4mb"}));
+app.use(cors()); 
+
+// Define a simple route
+
+app.use("/api/status", (req, res) => res.send("Server is running"));
+
+// Start the server
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("server is running on : "+ PORT));
+server.listen(PORT, () => console.log("Server is Running on Port :" + PORT));
