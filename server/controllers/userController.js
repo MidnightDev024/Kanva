d3f163adc26d28b0b629d4362862cc1db2fb7707
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-29 16:54:10 +0530

    refactored:userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import { TbPasswordUser } from "react-icons/tb";
 import User from "./models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
 
 //  SignUp a new user
 
@@ -57,3 +58,31 @@ export const login = async (req, res) => {
         res.json({success: false, message: error.message}); 
     }
 };
+
+// controller to check if user is authenticated
+
+export const checkAuth = async (req, res) => {
+    res.json({success: true, user: req.user});
+}
+
+// controller to update user profile details
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic, bio, fullName } = req.body;
+        const userId = req.user._id;
+        let updatedUser;
+
+        if(!profilePic){
+            updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName}, {new: true});
+        }else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName}, {new: true});
+        }
+        res.json({success: true, userData: updatedUser});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message}); 
+    }
+}
