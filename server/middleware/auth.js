c64c9135cb9e20d91e08c984abfba507cd55f385
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-29 16:54:34 +0530

    refactored: auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,7 +9,16 @@ export const protectRoute = async (req, res, next)=>{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(decoded.userId).select("-password");
 
+        if(!user){ 
+            return res.json({success: false, message: "User not found"});
+        }
+
+        req.user = user;
+        next();
+
     } catch (error) {
+        console.log(error.message);
         
+        return res.json({success: false, message: error.message});
     }
 }
