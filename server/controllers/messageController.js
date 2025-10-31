8f5b433fc0482265b8e3a1c540b439ca9331f4e3
MidnightDev024 <chawlaakash024@gmail.com>
2025-10-31 23:35:49 +0530

    refactored:messageController.js

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -22,3 +22,36 @@ export const getUsersForSidebar = async (req, res)=> {
         res.json({success: false, message: error.message})
     }
 }
+
+// get all messages for selected user
+export const getMessages = async (req, res) => {
+    try { 
+        const { id: selectedUserId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: myId}
+            ]
+        });
+        await Message.updateMany({senderId: selectedUserId, receiverId: myId}, {seen: true})
+
+        res.json({success: true, messages})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// api to mark messages as seen using message ids
+export const markMessagesAsSeen = async (req, res) => {
+    try {
+        const { messageIds } = req.body;
+        await Message.updateMany(_id, {seen: true});
+        res.json({success: true});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+} 
