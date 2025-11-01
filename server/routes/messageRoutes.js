7eb1250c44be2e18ffc6da2d19721159071041b4
MidnightDev024 <chawlaakash024@gmail.com>
2025-11-01 22:12:06 +0530

    refactored: messageRoutes.js

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { protectRoute } from '../middlewares/auth.js';
+import { protectRoute } from '../middleware/auth.js';
 import {getMessages, getUsersForSidebar, markMessagesAsSeen, sendMessage} from '../controllers/messageController';
 
 const messageRouter = express.Router();
