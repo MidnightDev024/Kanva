e17fb80840c9f7926a26d49e835b1907df43a4cd
MidnightDev024 <chawlaakash024@gmail.com>
2025-11-13 20:19:20 +0530

    chatContaineer.jsx

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef } from 'react';
-import assets, { messagesDummyData, userDummyData } from '../assets/assets';
-import { formateMessageTime } from '../library/utils';
-import { chatContext } from '../context/chatContext.jsx';
-import { authContext } from '../../context/authContext';
+import assets, { messagesDummyData, userDummyData } from '../assets/assets.js';
+import { formateMessageTime } from '../library/utils.js';
+import { chatContext } from "../../context/chatContext.jsx";
+import { authContext } from '../../context/authContext.jsx';
 
 const ChatContainer = () => {
 
@@ -14,6 +14,28 @@ const ChatContainer = () => {
 
   const [input, setInput] = React.useState('');
 
+  // handle send message
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    if(input.trim() === "") return null;
+    await sendMessage({text : input.trim()});
+    setInput('');
+  }
+
+  // Handle sending an image
+  const handleSendImage = async (e) => {
+    const file = e.target.files[0];
+    if(!file || !file.type.startsWith('image/')){
+      toast.error('Please select a valid image file');
+    }
+    const reader = new FileReader();
+    reader.onloadend = async () => {
+      await sendMessage({image: reader.result});
+      e.target.value = "";
+    }
+    reader.readAsDataURL(file);
+  }
+
   useEffect(()=>{
     if(scrollEnd.current){
       scrollEnd.current.scrollIntoView({ behaviour: "smooth"})
@@ -24,7 +46,7 @@ const ChatContainer = () => {
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
       {/* ---------- Header -------------- */}
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
-        <img src={selectedUser.profilePic} alt="User Profile Pic" className='w-8 rounded-full'/>
+        <img src={selectedUser.profilePic || assets.avatar_icon} alt="User Profile Pic" className='w-8 rounded-full'/>
         <p className='flex-1 text-lg text-white flex items-center gap-2'>
           {selectedUser.fullName}
           <span className='w-2 h-2 rounded-full bg-green-500'></span>
@@ -52,8 +74,8 @@ const ChatContainer = () => {
       {/* --------- bottom area --------- */}
       <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
         <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
-          <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Sent a message...' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
-          <input type="file" id="image" accept='image/png, image/jpeg' hidden />
+          <input onChange={(e) => setInput(e.target.value)} value={input} onKeyDown={(e)=> e.key === "Enter" ? handleSendMessage(e) : null} type="text" placeholder='Sent a message...' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+          <input onChange={handleSendImage} type="file" id="image" accept='image/png, image/jpeg' hidden />
           <label htmlFor="image">
             <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' /> 
           </label>
@@ -63,10 +85,10 @@ const ChatContainer = () => {
     </div>
   ) : (
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
-      <img src={assets.logo_icon} alt=""  className='max-w-16' />
+      <img onClick={handleSendMessage} src={assets.logo_icon} alt=""  className='max-w-16' />
       <p className='text-lg font-medium text-white'>JUST KANVA IT...</p>
     </div>
   )
 }
 
-export default ChatContainer
+export default ChatContainer;
