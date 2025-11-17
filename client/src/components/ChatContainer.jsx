8b94ae8fc7167badc38a019eb22ca24af0661b17
MidnightDev024 <chawlaakash024@gmail.com>
2025-11-17 18:08:55 +0530

    refactored : ChatContainer.jsx

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -53,7 +53,7 @@ const ChatContainer = () => {
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
       {/* ---------- Header -------------- */}
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
-        <img src={selectedUser.profilePic || assets.avatar_icon} alt="User Profile Pic" className='w-8 rounded-full'/>
+        <img src={selectedUser.profilePicture || assets.avatar_icon} alt="User Profile Pic" className='w-8 rounded-full'/>
         <p className='flex-1 text-lg text-white flex items-center gap-2'>
           {selectedUser.fullName}
           {onlineUsers.includes(selectedUser._id) && <span className='w-2 h-2 rounded-full bg-green-500'></span>}
@@ -63,7 +63,7 @@ const ChatContainer = () => {
       </div>
       {/* ---------- chat area ------------ */}
       <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
-        {messages.map((msg, index)=> (
+        {messages.filter(Boolean).map((msg, index)=> (
           <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== authUser._id && 'flex-row-reverse'}`}>
             {msg.image ? (
               <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' />
@@ -71,7 +71,7 @@ const ChatContainer = () => {
               <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId !== authUser._id ? 'rounded-bg-none' : 'rounded-bl-none'}`}>{msg.text}</p>
             )}
             <div className='text-center text-xs'>
-              <img src={msg.senderId === authUser._id ? authUser?.profilePic || assets.avatar_icon : selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-7 rounded-full' />
+              <img src={msg.senderId === authUser._id ? authUser?.profilePicture || assets.avatar_icon : selectedUser?.profilePicture || assets.avatar_icon} alt="" className='w-7 rounded-full' />
               <p className='text-gray-500'>{formateMessageTime(msg.createdAt)}</p>
             </div>
           </div>
