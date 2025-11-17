94074b9d410852cf0a240066da803a9b446a9bc5
MidnightDev024 <chawlaakash024@gmail.com>
2025-11-17 21:21:16 +0530

    refactored : authContext.jsx

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-import { set } from "mongoose";
 
 const backendURL = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.baseURL = backendURL;
@@ -100,7 +99,7 @@ export const AuthProvider = ({ children }) => {
         newSocket.connect();
         setSocket(newSocket);
         
-        newSocket.on("online-users", (userIds) => {
+        newSocket.on("getOnlineUsers", (userIds) => {
             setOnlineUsers(userIds);
         });
     }
